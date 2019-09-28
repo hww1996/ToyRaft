0b2312aaf9cdb87a487c9666e71ca9ed734f130b
hww1996 <qq1143107001@msn.com>
2019-09-28 16:44:06 +0800

    添加raft基本类型

--- include/raft.h
+++ include/raft.h
@@ -1,27 +1,80 @@
 //
 // Created by hww1996 on 2019/9/28.
 //
+#include <vector>
+#include <memory>
+#include <unordered_map>
 
 #ifndef TOYRAFT_RAFT_H
 
-class request_vote{
+namespace ToyRaft{
+
+class RequestVote{
 private:
     int term;
-    int candidate_id;
-    int last_log_term;
-    int last_commit_log_index;
+    int candidateId;
+    int lastLogTerm;
+    int lastCommitLogIndex;
 };
 
-class request_vote_response{
+class RequestVoteResponse{
 private:
     int term;
-    int vote_for;
+    bool voteForMe;
 };
 
-class request_append{
+typedef char* LogType;
+
+class RequestAppend{
 private:
-    
+    int term;
+    int preLogTerm;
+    int preLogIndex;
+    int leaderCommit;
+    std::vector<LogType> entries;
 };
+
+class RequestAppendResponse{
+private:
+    int term;
+    bool success;
+    int currentIndex;
+    int firstAppliedIndex;
+};
+
+enum Status{
+    FOLLOWER,
+    CANDIDATE,
+    LEADER,
+};
+
+class Server{
+private:
+    int id;
+    int Term;
+    int votedFor;
+
+    std::vector<LogType> log;
+    int commitIndex;
+    int lastAppliedIndex;
+
+    Status state;
+
+    /* 计时器，周期函数每次执行时会递增改值 */
+    int timeoutElapsed;
+
+    std::unordered_map<std::unique_ptr<Server>> nodes;
+    std::vector<int> nodeApplied;
+    std::vector<int> nodeCommited;
+
+    int electionTimeout;
+    int requestTimeout;
+
+    int  currentLeader;
+};
+
+
+} // namespace ToyRaft
 
 #define TOYRAFT_RAFT_H
 
