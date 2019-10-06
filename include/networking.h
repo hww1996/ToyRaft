6cc93c33dc7c1231dda04ee2e187cfc21c72d81b
hww1996 <qq1143107001@msn.com>
2019-10-06 16:41:21 +0800

    使用grpc (#11)
    异步使用grpc

--- include/networking.h
+++ include/networking.h
@@ -7,25 +7,27 @@
 #include <thread>
 #include <mutex>
 #include <unordered_map>
+#include <memory>
 
 #include "raft.pb.h"
+#include "raft.grpc.pb.h"
 
 #ifndef TOYRAFT_NET_H
 #define TOYRAFT_NET_H
 namespace ToyRaft {
     struct NetData {
-        int64_t id;
-        std::string buf;
+        int64_t id_;
+        ::ToyRaft::AllSend buf_;
+        NetData(int64_t id,const ::ToyRaft::AllSend &allSend);
     };
 
     class RaftNet {
     public:
         RaftNet();
-
         // 从网络中获取数据
         static int sendToNet(int64_t, ::ToyRaft::AllSend &);
 
-        static int recvFromNet(::ToyRaft::AllSend &);
+        static int recvFromNet(::ToyRaft::AllSend *);
 
     private:
         static int realSend();
@@ -34,7 +36,8 @@ namespace ToyRaft {
 
         static std::deque<::ToyRaft::NetData> recvBuf;
         static std::deque<::ToyRaft::NetData> sendBuf;
-        static std::unordered_map<int, int> sendIdMapping;
+        static std::unordered_map<int,
+                std::unique_ptr<::ToyRaft::SendAndReply::Stub>> sendIdMapping;
         static std::thread recvThread;
         static std::thread sendThread;
         static std::mutex recvMutex;
--- src/networking.cpp
+++ src/networking.cpp
@@ -5,6 +5,8 @@
 #include "networking.h"
 
 namespace ToyRaft {
+    NetData::NetData(int64_t id, const ::ToyRaft::AllSend &allSend) : id_(id), buf_(allSend){}
+
     std::deque<::ToyRaft::NetData> RaftNet::recvBuf;
     std::deque<::ToyRaft::NetData> RaftNet::sendBuf;
     std::mutex RaftNet::recvMutex;
@@ -12,5 +14,29 @@ namespace ToyRaft {
     std::thread RaftNet::recvThread(RaftNet::realRecv);
     std::thread RaftNet::sendThread(RaftNet::realSend);
 
+    RaftNet::RaftNet() {
+        recvThread.detach();
+        sendThread.detach();
+    }
+
+    int RaftNet::sendToNet(int64_t id, ::ToyRaft::AllSend &allSend) {
+        int ret = 0;
+        NetData netData(id, allSend);
+        {
+            std::lock_guard<std::mutex> lock(sendMutex);
+            sendBuf.push_back(netData);
+        }
+        return ret;
+    }
+
+    int RaftNet::recvFromNet(::ToyRaft::AllSend *allSend) {
+        int ret = 0;
+        {
+            std::lock_guard<std::mutex> lock(sendMutex);
+            *allSend = recvBuf.front().buf_;
+            recvBuf.pop_front();
+        }
+        return ret;
+    }
 
 } // namespace ToyRaft
